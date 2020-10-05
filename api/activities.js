6233370758ce319766be50559f6b4c588ed867a7
Preston Wallace <wallace.preston@gmail.com>
2020-10-04 23:49:00 -0700

    feat(requiredNotSent): Add middleware for undeinfeds in req.body

--- api/activities.js
+++ api/activities.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getAllActivities, createActivity, updateActivity, getPublicRoutinesByActivity } = require('../db');
-const { requireUser } = require('./utils')
+const { getAllActivities, getActivityById, createActivity, updateActivity, getPublicRoutinesByActivity } = require('../db');
+const { requireUser, requiredNotSent } = require('./utils')
 
 // GET /activities/:activityId/routines
 router.get('/:activityId/routines', async (req, res, next) => {
   try {
     const routines = await getPublicRoutinesByActivity({id: req.params.activityId});
-    res.send(routines);
+    if(routines) {
+      res.send(routines);
+    } else {
+      next({
+        name: 'NotFound',
+        message: `No Routines found for Activity ${req.params.activityId}`
+      })
+    }
   } catch (error) {
     next(error);
   }
@@ -24,22 +31,46 @@ router.get('/', async (req, res, next) => {
 })
 
 // POST /activities
-router.post('/', requireUser, async (req, res, next) => {
+router.post('/', requireUser, requiredNotSent({requiredParams: ['name', 'description']}), async (req, res, next) => {
   try {
     const {name, description} = req.body;
     const createdActivity = await createActivity({name, description});
-    res.send(createdActivity)
+    if(createdActivity) {
+      res.send(createdActivity);
+    } else {
+      next({
+        name: 'FailedToCreate',
+        message: 'There was an error creating your activity'
+      })
+    }
   } catch (error) {
     next(error);
   }
 });
 
 // PATCH /activities/:activityId
-router.patch('/:activityId', requireUser, async (req, res, next) => {
+router.patch('/:activityId', requireUser, requiredNotSent({requiredParams: ['name', 'description'], atLeastOne: true}), async (req, res, next) => {
   try {
-    const {name, description} = req.body;
-    const updatedActivity = await updateActivity({id: req.params.activityId, name, description})
-    res.send(updatedActivity);
+    const {activityId} = req.params;
+    const existingActivity = await getActivityById(activityId);
+    console.log('existingActivity: ', existingActivity);
+    if(!existingActivity) {
+      next({
+        name: 'NotFound',
+        message: `No activity by ID ${activityId}`
+      });
+    } else {
+      const {name, description} = req.body;
+      const updatedActivity = await updateActivity({id: activityId, name, description})
+      if(updatedActivity) {
+        res.send(updatedActivity);
+      } else {
+        next({
+          name: 'FailedToUpdate',
+          message: 'There was an error updating your activity'
+        })
+      }
+    }
   } catch (error) {
     next(error);
   }
--- api/utils.js
+++ api/utils.js
@@ -9,6 +9,43 @@ function requireUser(req, res, next) {
   next();
 }
 
+// takes required parameters as an array, returns a middleware function that sends back a message if they're not present
+const requiredNotSent = ({ requiredParams, atLeastOne = false }) => {
+  return (req, res, next) => {
+    // for operations that need at least one param. Not all required.
+    if(atLeastOne) {
+      let numParamsFound = 0;
+      for(let param of requiredParams) {
+        if(req.body[param]) {
+          numParamsFound++;
+        }
+      }
+      if(!numParamsFound) {
+        next({
+          name: 'MissingParams',
+          message: `Must provide at least one of these in body: ${requiredParams.join(', ')}`
+        })
+      } else {
+        next();
+      }
+    } else {
+      // figure out which ones are not defined, and return them
+      const notSent = [];
+      for(let param of requiredParams) {
+        if(!req.body[param]) {
+          notSent.push(param);
+        }
+      }
+      if(notSent.length) next({
+        name: 'MissingParams',
+        message: `Required Parameters not sent in body: ${notSent.join(', ')}`
+      })
+      next();
+    }
+  }
+}
+
 module.exports = {
-  requireUser
+  requireUser,
+  requiredNotSent,
 }
